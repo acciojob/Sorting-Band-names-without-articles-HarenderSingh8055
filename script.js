c0a732d1592f2d3e126ddc1cac9c6af49544b5dc
HarenderSingh8055 <124910145+HarenderSingh8055@users.noreply.github.com>
2023-03-30 20:30:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
 
+const bands = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+function strip(bandName) {
+	return bandName.replace(/^(a |the |an)/i, '').trim();
+	
+}
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+document.querySelector('#band').innerHTML = 
+	sortedBands
+    .map(band => `<li>${band}</li>`)
+.join('')
+
+console.log(sortedBands);
